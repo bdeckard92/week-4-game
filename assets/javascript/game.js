bad308554fbc1b1c4c8b6fb117e9fb9703bdcd54
Bob Deckard <bdeckard92@gmail.com>
2016-11-17 18:09:21 -0600

    good try

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,132 @@
+console.log (5+6);
+function numberRange(min, max) {
+    var ranNum = Math.floor((Math.random() * (max - min + 1) + min));
+    return ranNum;
+}
+var compNum = numberRange(19, 120);
+
+var totalPoints = 0;
+
+var yourNumber = 0;
+var wins = 0;
+var losses = 0;
+var blue = numberRange(1, 12);
+var purple = numberRange(1, 12);
+var yellow = numberRange(1, 12);
+var pink = numberRange(1, 12);
+
 $(document).ready(function() {
 
-console.log(5+6);
+    $("#number").text(totalPoints);
+
+    $("#numberToGuess").text(compNum);
+
+    $("#wins").text(wins);
+
+    $("#loss").text(losses);
+
+    
+
+    $("#bluepony").on("click", function() {
+
+        totalPoints += blue;
 
 
-	var ponies = ['<img src="assets/images/pinkpony.png">','<img src="assets/images/bluepony.png">','<img src="assets/images/purplepony.png">','<img src="assets/images/yellowpony.png">']
-	var num_lives = ""
+        $("#number").text(totalPoints);
+
+        
+
+        winLoss(totalPoints, compNum);
 
 
-for (i = 0; i < ponies.length; i++){	
-	var ponieBtn = $("<button data-pony=" + ponies[i] + ">");
-          console.log(ponieBtn);
-        // 3. Then give each "letterBtn" the following classes: "letter-button" "letter" "letter-button-color".
-        ponieBtn.addClass("ponie-button");
-        ponieBtn.addClass("ponie");
-        ponieBtn.addClass("ponie-button-color"); 
-		ponieBtn.text(letters[i]);
-        // 6. Finally, append each "letterBtn" to the "#buttons" div (provided).
-        $("#number").append(ponieBtn);
-}
+    });
+
+
+
+
+    $("#purplepony").on("click", function() {
+
+        totalPoints += purple;
+
+        $("#number").text(totalPoints);
+
+       
+
+        winLoss(totalPoints, compNum);
+
+    });
+
+
+
+    $("#yellowpony").on("click", function() {
+
+        totalPoints += yellow;
+
+        $("#number").text(totalPoints);
+
+       
+
+        winLoss(totalPoints, compNum);
+
+
+    });
+
+    $("#pinkpony").on("click", function() {
+
+        totalPoints += pink;
+
+        $("#number").text(totalPoints);
+
+        
+
+        winLoss(totalPoints, compNum);
+
+    });
+
+
+});
+
+
+function reset() {
+     blue = numberRange(1, 12);
+     purple = numberRange(1, 12);
+     yellow = numberRange(1, 12);
+     pink = numberRange(1, 12)
+
+    compNum= numberRange(19, 120);
+
+    playerNum = 0;
+    
+    $("#number").text(totalPoints);
+
+    $("#numberToGuess").text(compNum);
+
+    
+};
+
+//reset not working... 
+
+function winLoss(playerNum, compNum) {
+
+    if (playerNum === compNum) {
+        alert("Pony Power!");
+        wins++;
+        totalPoints = 0;
+        $("#wins").text(wins);
+        reset();
+    };
+
+    if (playerNum > compNum) {
+        alert("Pony Punked!");
+        losses++;
+        totalPoints = 0;
+        $("#loss").text(losses);
+        reset();
+    };
+
+//add music, fix reset
+
+};
 
 
 
